7a865a3e974a72fc53339aeac9a24ec2fd13952f
mjoseph96 <melvin.joseph@sdc.co.uk>
2019-10-28 16:30:54 +0000

    Add project files.

---
+++ ExportToExcel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+using _excel = Microsoft.Office.Interop.Excel;
+namespace ExportToExcel
+{
+    class Program
+    {
+        string path = "";
+        _Application excel = new _excel.Application();
+        public Workbook wb;
+        public Worksheet ws;
+
+
+        public _Workbook Workbook;
+       public Program(string path, string sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].ToString()==null)
+            {
+                return ws.Cells[i, j].ToString();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
